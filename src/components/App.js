220af7184312d8b9af915e1023ef75d83eadfece
jsahlsa <joe.sahlstrom@gmail.com>
2021-09-03 20:11:08 +0000

    🎉 pretty good, pretty, pretty, pretty good

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import img from '../img/j.svg';
+
+export default function App() {
+  return (
+    <div className='App'>
+      <img src={img} alt="big letter J" />
+      
+      <h1>Base React App</h1>
+      <h3>
+        Edit <code>App.js</code> to get started
+      </h3>
+    </div>
+  );
+}
